c12e06a9b8e01d6230ea1d9cc1f70f45738c1c60
Ori Markowitz <ori.markowitz@gmail.com>
2020-12-14 18:16:18 -0500

    finished validations in controller actions lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validates :content, length: {minimum: 100}
 end
